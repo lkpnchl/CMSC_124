1851e911ed9c00af5e4daf16aa2784acbf02d246
Gee Nine <69750024+lkpnchl@users.noreply.github.com>
2023-12-05 20:03:10 +0800

    added open file dialog (still needs fixing)

--- IDE/MainIDE.gd
+++ IDE/MainIDE.gd
@@ -6,18 +6,17 @@ var code_edit : CodeEdit  # Reference to the CodeEdit instance
 func _ready():
 	# Load references to FileDialog and CodeEdit
 	file_dialog = $OpenFileDialog
-	code_edit = $CodeEdit
+	code_edit = get_node("TextureRect/PanelContainer2/MarginContainer/VBoxContainer/PanelContainer/CodeEdit")
 
 	# Connect signals
 	connect_signals()
 
 func connect_signals():
 	# Connect the Open File button pressed signal to a function
-	var open_button : Button = $Open
-	#open_button.connect("pressed", self, "_on_open_pressed")
-
+	var open_button : TextureButton = get_node_or_null("TextureRect/PanelContainer2/MarginContainer/VBoxContainer/hbOX/Open")
+	open_button.pressed.connect(_on_open_pressed)
+	file_dialog.file_selected.connect(_on_file_selected)
 	# Connect the FileDialog's file_selected signal to a function
-	#file_dialog.connect("file_selected", self, "_on_file_selected")
 
 # Function called when the Open File button is pressed
 func _on_open_pressed() -> void:
@@ -27,13 +26,14 @@ func _on_open_pressed() -> void:
 # Function called when a file is selected in the FileDialog
 func _on_file_selected(path: String) -> void:
 	# Read the file and set the content in the CodeEdit
-	var file : File = File.new()
-	if file.file_exists(path) and file.open(path, File.READ) == OK:
-		var content : String = file.get_as_text()
-		file.close()
-		code_edit.text = content
-	else:
-		show_error("Failed to load the selected file.")
+#	var file : File = File.new()
+#	if file.file_exists(path) and file.open(path, File.READ) == OK:
+#		var content : String = file.get_as_text()
+#		file.close()
+#		code_edit.text = content
+#	else:
+#		show_error("Failed to load the selected file.")
+	pass
 
 func show_error(message: String) -> void:
 	# Display the error to the user, perhaps with a Popup or Label
